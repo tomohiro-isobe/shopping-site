b6f8a178b4cd8b76b09bba25542e2f3d049633ed
EC2 Default User <ec2-user@ip-172-31-44-2.us-east-2.compute.internal>
2018-07-06 04:48:45 +0000

    click_add

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ $(function() {
         dots: true,
         items: 1,
         autoplay: true
-        
+    });
+    
+    $('.sp_menu').click(function() {
+        $('.sp_menu_li').slideToggle();
+        $('.sp_menu_li').css('background-color', '#f9f8f6');
     });
 });
